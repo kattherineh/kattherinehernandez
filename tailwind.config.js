26271e65f5ef2f5f6e145b76a2d63e3699653e60
katerinmayely <katerinmayely@gmail.com>
2024-09-25 06:34:59 -0600

    avance en página principal

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,11 @@ export default {
     extend: {
       colors:{
         'dark-blue': '#11071F',
-        'header-blue': '#1A0B2E',
+        'header-footer-blue': '#1A0B2E',
         'clear': '#E4E1EE',
         'hover': '#4c187754',
-        'big-letter': '#698aa9'
+        'big-letter': '#698aa9',
+        'ligth-purple': '#e7dcf8d2'
       },
       fontFamily: {
         preahvihear: ["Preahvihear", "sans-serif"]
